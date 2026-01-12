8a6d3a23518a5b555f9180528ccce8addae21455
Oleksii Cherep <alex1998120850@gmail.com>
2026-01-12 21:50:51 +0200

    Update votes.ts

--- src/components/types/votes.ts
+++ src/components/types/votes.ts
@@ -1,7 +1,7 @@
-export type VoteType = 'good' | 'neutral' | 'bad'
+export type VoteType = 'good' | 'neutral' | 'bad';
 
 export interface Votes {
-	good: number;
+    good: number;
     neutral: number;
     bad: number;
 }
