c1614a1af7ba6ca3588c7253ed68eca408ef0db0
Oleksii Cherep <alex1998120850@gmail.com>
2026-01-12 19:45:41 +0200

    Update App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,19 +1,11 @@
-import Product from "../Product";
+import css from './App.module.css';
+import CafeInfo from '../cafeInfo/CafeInfo';
 
-export default function App() {
-  return (
-    <>
-      <h1>Best selling</h1>
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
-        price={14.29}
-      />
-    </>
-  );
-}
+const App = () => {
+  return (<div className={css.app}>
+  <CafeInfo /> 
+</div>
+  )
+};
+
+export default App;
