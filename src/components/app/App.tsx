9aac515eced8b7b2227847f84f5c42fa70f7d5b2
Oleksii Cherep <alex1998120850@gmail.com>
2026-01-19 09:54:52 +0200

    Update App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import css from './App.module.css';
-import CafeInfo from '../cafeInfo/CafeInfo';
-import VoteOptions from '../voteOptions/VoteOptions';
-import VoteStats from '../voteStats/VoteStats';
-import Notification from '../notification/Notification';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 import type { Votes, VoteType } from '../../types/votes';
 
 const App = () => {
