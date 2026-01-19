6329573eaf7820cb0fc0967cd6cb28efbd0e9f5d
Oleksii Cherep <alex1998120850@gmail.com>
2026-01-19 10:05:37 +0200

    Update App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import css from './App.module.css';
+import css from './app.module.css';
 import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
